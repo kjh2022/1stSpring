4326000d18776536ffd8a7969da1a375f35d0a48
kjh2022 <fhqlxm123@daum.net>
2022-10-19 20:46:54 +0900

    1019 MVC모델 구성

---
+++ ex00/src/main/java/com/yedam/java/emp/service/EmpVO.java
@@ -0,0 +1,22 @@
+package com.yedam.java.emp.service;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class EmpVO {
+	
+	private int employeeId; //employee_id
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private Date hireDate;
+	private String jobId;
+	private double salary;
+	private double commissionPct;
+	private int managerId;
+	private int departmentId;
+		
+}
